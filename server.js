039f560052e5fadb889d3f226e60aa6d23d54951
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-12-02 19:44:02 +0700

    fix: allow headers and optionsSuccessStatus 200

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
