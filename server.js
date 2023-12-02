32848f2517c693f3bc3bd10cd5285d376070b674
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-12-02 16:58:08 +0700

    fix: add method patch

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 };
 app.use(cors(corsOptions));
 
